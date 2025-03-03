cd6303d6fab4d303726a10661d993cd10a21232c
Daki <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2025-03-03 09:40:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# This program "blinks" LED on for one second and off for one second
+#
+# Created by: Daki Anrango-Boada
+# Created on: March 2025
+
+import time
+import board
+import digitalio
+
+# Set LED to output
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+# Looping LED on for one second and off for one second
+while True:
+    led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
 
